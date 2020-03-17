727bfe0b370467b2587cbadb2f262b6d84bb3f53
awiercinska <awiercinska@xentivo.pl>
2020-03-17 09:04:49 +0100

    commiting cw03

---
+++ apbdCW3/apbdCW3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace apbdCW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
